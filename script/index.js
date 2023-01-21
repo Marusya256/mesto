1db98e4132389c1cad52fe07264e7c50b6589868
Marina Galamdinova <mgalamdinova@gmail.com>
2023-01-21 06:46:04 +0300

    fix: исправление ошибо в вёрстке, стилях и js

--- script/index.js
+++ script/index.js
@@ -1,11 +1,15 @@
-let buttonEdit = document.querySelector('.button-edit');
+let buttonEdit = document.querySelector('.button_type_edit');
 let popup = document.querySelector('.popup');
-let buttonClose = document.querySelector('.button-close');
-let userName = infoname.textContent;
-let aboutUser = infoabout.textContent;
+let buttonClose = document.querySelector('.button_type_close');
 
 function openPopup() {
     popup.classList.add('popup_opened');
+
+    let userName = infoname.textContent;
+    let aboutUser = infoabout.textContent;
+
+    document.getElementById("username").value = userName;
+    document.getElementById("aboutuser").value = aboutUser;
 }
 
 function closePopup() {
@@ -17,16 +21,9 @@ buttonClose.addEventListener('click', closePopup);
 
 
 
-document.getElementById("username").value = userName;
-document.getElementById("aboutuser").value = aboutUser;
-
-
-
-
-
 let formElement = document.querySelector('.popup__container'); 
-let nameInput = document.querySelector('.input-username'); 
-let jobInput = document.querySelector('.input-aboutuser'); 
+let nameInput = document.querySelector('.popup__input_user_name'); 
+let jobInput = document.querySelector('.popup__input_user_about'); 
 
 function handleFormSubmit (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
@@ -39,7 +36,7 @@ function handleFormSubmit (evt) {
     document.getElementById("infoname").textContent = nameU;
     document.getElementById("infoabout").textContent = jobU;
 
-    popup.classList.remove('popup_opened');
+    closePopup();
 
 }
 
